9176514d4006c02d90c64bced111c0031ad8acd3
Srinnix <srinnix13@gmail.com>
2016-04-24 23:52:37 +0700

    appController

---
+++ app/src/main/java/com/qtd/weatherforecast/AppController.java
@@ -0,0 +1,60 @@
+package com.qtd.weatherforecast;
+
+import android.app.Application;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Dell on 4/24/2016.
+ */
+public class AppController extends Application {
+    public static final String TAG = AppController.class.getSimpleName();
+    private RequestQueue requestQueue;
+    private ImageLoader imageLoader;
+
+    private static AppController instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+
+    public static synchronized AppController getInstance() {
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public ImageLoader getImageLoader() {
+        getRequestQueue();
+        if (imageLoader == null) {
+            imageLoader = new ImageLoader(requestQueue, new LruBitmapCache());
+        }
+        return imageLoader;
+    }
+
+    public void addToRequestQueue(Request request, String tag) {
+        request.setTag(TAG);
+        getRequestQueue().add(request);
+    }
+
+    public void addToRequestQueue(Request request) {
+        request.setTag(TAG);
+        getRequestQueue().add(request);
+    }
+
+    public void cancelPendingRequest(Object tag) {
+        if (requestQueue != null) {
+            requestQueue.cancelAll(tag);
+        }
+    }
+}
