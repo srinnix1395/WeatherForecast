88b8eafce90e80264173c98f099a8534d7794cce
tuha <srinnix13@gmail.com>
2016-12-22 12:18:12 +0700

    indicator

--- pageindicator/src/main/java/com/example/pageindicator/IconCirclePageIndicator.java
+++ pageindicator/src/main/java/com/example/pageindicator/IconCirclePageIndicator.java
@@ -17,191 +17,159 @@ import java.util.ArrayList;
  */
 
 public class IconCirclePageIndicator extends View implements PageIndicator, ViewPager.OnPageChangeListener {
-	public static final int DEFAULT_RADIUS = 10;
-	public static final int DEFAULT_INIT_POSITION = 0;
-	public static final int DEFAULT_SELECTED_COLOR = Color.WHITE;
-	public static final int DEFAULT_UNSELECTED_COLOR = Color.WHITE;
-	public static final int DISTANCE = 30;
-	public static final int ALPHA_SELECTED = 255;
-	public static final int ALPHA_UNSELECTED = 70 * 255 / 100;
-	private static final int DEFAULT_POSITION_ICON = 0;
-	
-	private int radius;
-	private int currentPosition;
-	private int beforePosition;
-	private int selectedColor;
-	private int unselectedColor;
-	private int selectedRes;
-	private int unselectedRes;
-	private int positionIcon = DEFAULT_POSITION_ICON;
-	
-	private ViewPager viewPager;
-	private ArrayList<Indicator> indicatorList;
-	private Paint paint;
-	
-	public IconCirclePageIndicator(Context context) {
-		super(context);
-		init();
-	}
-	
-	public IconCirclePageIndicator(Context context, @Nullable AttributeSet attrs) {
-		super(context, attrs);
-		TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.IconCirclePageIndicator);
-		
-		beforePosition = DEFAULT_INIT_POSITION;
-		currentPosition = DEFAULT_INIT_POSITION;
-		radius = typedArray.getDimensionPixelSize(R.styleable.IconCirclePageIndicator_radius, DEFAULT_RADIUS);
-		selectedColor = typedArray.getColor(R.styleable.IconCirclePageIndicator_colorSelected, DEFAULT_SELECTED_COLOR);
-		unselectedColor = typedArray.getColor(R.styleable.IconCirclePageIndicator_colorUnselected, DEFAULT_UNSELECTED_COLOR);
-		selectedRes = typedArray.getResourceId(R.styleable.IconCirclePageIndicator_selectedResource, -1);
-		unselectedRes = typedArray.getResourceId(R.styleable.IconCirclePageIndicator_unselectedResource, -1);
-		
-		typedArray.recycle();
-		init();
-	}
-	
-	private void init() {
-		paint = new Paint();
-		paint.setAntiAlias(true);
-	}
-	
-	@Override
-	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		int desiredHeight = 2 * radius;
-		
-		int width;
-		int height;
-		
-		int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-		int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-		int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-		int heightSize = MeasureSpec.getSize(heightMeasureSpec);
-		
-		if (widthMode == MeasureSpec.EXACTLY) {
-			width = widthSize;
-		} else if (widthMode == MeasureSpec.AT_MOST) {
-			width = widthSize;
-		} else {
-			width = 0;
-		}
-		
-		if (heightMode == MeasureSpec.EXACTLY) {
-			height = heightSize;
-		} else if (heightMode == MeasureSpec.AT_MOST) {
-			height = Math.min(desiredHeight, heightSize);
-		} else {
-			height = desiredHeight;
-		}
-		
-		setMeasuredDimension(width, height);
-	}
-	
-	@Override
-	protected void onDraw(Canvas canvas) {
-		if (viewPager == null) {
-			return;
-		}
-		
-		for (Indicator indicator : indicatorList) {
-			indicator.draw(canvas, paint);
-		}
-	}
-	
-	@Override
-	public void setupWithViewPager(ViewPager viewPager) throws AdapterNotFoundException {
-		setupWithViewPager(viewPager, DEFAULT_INIT_POSITION);
-	}
-	
-	@Override
-	public void setupWithViewPager(ViewPager viewPager, int initialPosition) throws AdapterNotFoundException {
-		if (this.viewPager == viewPager) {
-			return;
-		}
-		if (viewPager.getAdapter() == null) {
-			throw new AdapterNotFoundException();
-		}
-		this.viewPager = viewPager;
-		this.viewPager.setCurrentItem(initialPosition, true);
-		this.viewPager.addOnPageChangeListener(this);
-		initIconCircle();
-		invalidate();
-	}
-	
-	private void initIconCircle() {
-		int count = viewPager.getAdapter().getCount();
-		indicatorList = new ArrayList<>();
-		
-		int sizeAll = count * radius + (count - 1) * DISTANCE;
-		float firstX = getWidth() / 2 - (sizeAll / 2);
-		float centerY = getHeight() / 2;
-		
-		for (int i = 0; i < count; i++) {
-			if (i == positionIcon) {
-				indicatorList.add(new Icon(positionIcon == currentPosition ? selectedRes : unselectedRes
-						, firstX, centerY - (radius / 2)
-						, radius, currentPosition == 0 ? ALPHA_SELECTED : ALPHA_UNSELECTED));
-			} else {
-				indicatorList.add(new Circle(firstX + (i + 0.5f) * radius + i * DISTANCE,
-						centerY, radius, i == currentPosition ? selectedColor : unselectedColor,
-						i == currentPosition ? ALPHA_SELECTED : ALPHA_UNSELECTED));
-			}
-		}
-	}
-	
-	@Override
-	public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-		
-	}
-	
-	@Override
-	public void onPageSelected(int position) {
-		beforePosition = currentPosition;
-		currentPosition = position;
-		
-		indicatorList.get(beforePosition).setAlpha(ALPHA_UNSELECTED);
-		indicatorList.get(currentPosition).setAlpha(ALPHA_SELECTED);
-		
-		if (beforePosition == positionIcon) {
-			((Icon) indicatorList.get(beforePosition)).setResourceId(unselectedRes);
-		} else {
-			((Circle) indicatorList.get(beforePosition)).setColor(unselectedColor);
-		}
-		
-		if (currentPosition == positionIcon) {
-			((Icon) indicatorList.get(beforePosition)).setResourceId(selectedRes);
-		} else {
-			((Circle) indicatorList.get(currentPosition)).setColor(selectedColor);
-		}
-		
-		invalidate();
-	}
-	
-	@Override
-	public void onPageScrollStateChanged(int state) {
-		
-	}
-	
-	public int getRadius() {
-		return radius;
-	}
-	
-	public void setRadius(int radius) {
-		this.radius = radius;
-	}
-	
-	public int getSelectedColor() {
-		return selectedColor;
-	}
-	
-	public void setSelectedColor(int selectedColor) {
-		this.selectedColor = selectedColor;
-	}
-	
-	public int getUnselectedColor() {
-		return unselectedColor;
-	}
-	
-	public void setUnselectedColor(int unselectedColor) {
-		this.unselectedColor = unselectedColor;
-	}
+    public static final String TAG = "indicatorCircle";
+
+    public static final int DEFAULT_RADIUS = 10;
+    public static final int DEFAULT_INIT_POSITION = 0;
+    public static final int DEFAULT_SELECTED_COLOR = Color.WHITE;
+    public static final int DEFAULT_UNSELECTED_COLOR = Color.WHITE;
+    public static final int DISTANCE = 30;
+    public static final int ALPHA_SELECTED = 255;
+    public static final int ALPHA_UNSELECTED = 50 * 255 / 100;
+    private static final int DEFAULT_POSITION_ICON = 0;
+
+    private int radius;
+    private int currentPosition;
+    private int selectedColor;
+    private int unselectedColor;
+    private int selectedRes;
+    private int unselectedRes;
+    private int positionIcon = DEFAULT_POSITION_ICON;
+
+    private ViewPager viewPager;
+    private ArrayList<Indicator> indicatorList;
+    private Paint paint;
+
+    public IconCirclePageIndicator(Context context) {
+        super(context);
+        init();
+    }
+
+    public IconCirclePageIndicator(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.IconCirclePageIndicator);
+
+        currentPosition = DEFAULT_INIT_POSITION;
+        radius = typedArray.getDimensionPixelSize(R.styleable.IconCirclePageIndicator_radius, DEFAULT_RADIUS);
+        selectedColor = typedArray.getColor(R.styleable.IconCirclePageIndicator_colorSelected, DEFAULT_SELECTED_COLOR);
+        unselectedColor = typedArray.getColor(R.styleable.IconCirclePageIndicator_colorUnselected, DEFAULT_UNSELECTED_COLOR);
+        selectedRes = typedArray.getResourceId(R.styleable.IconCirclePageIndicator_selectedResource, -1);
+        unselectedRes = typedArray.getResourceId(R.styleable.IconCirclePageIndicator_unselectedResource, -1);
+        positionIcon = typedArray.getInteger(R.styleable.IconCirclePageIndicator_positionIcon, 0);
+
+        typedArray.recycle();
+        init();
+    }
+
+    private void init() {
+        paint = new Paint();
+        paint.setAntiAlias(true);
+
+        indicatorList = new ArrayList<>();
+    }
+
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        initIconCircle();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (viewPager == null) {
+            return;
+        }
+
+        if (viewPager.getAdapter().getCount() <= 0) {
+            return;
+        }
+
+        for (Indicator indicator : indicatorList) {
+            indicator.draw(canvas, paint);
+        }
+    }
+
+    @Override
+    public void setupWithViewPager(ViewPager viewPager) throws AdapterNotFoundException {
+        setupWithViewPager(viewPager, DEFAULT_INIT_POSITION);
+    }
+
+    @Override
+    public void setupWithViewPager(ViewPager viewPager, int initialPosition) throws AdapterNotFoundException {
+        if (this.viewPager == viewPager) {
+            return;
+        }
+        if (viewPager.getAdapter() == null) {
+            throw new AdapterNotFoundException();
+        }
+        this.viewPager = viewPager;
+        this.viewPager.setCurrentItem(initialPosition, true);
+        this.viewPager.addOnPageChangeListener(this);
+    }
+
+    private void initIconCircle() {
+        int count = viewPager.getAdapter().getCount();
+
+        int sizeAll = count * radius + (count - 1) * DISTANCE;
+        float firstX = getWidth() / 2 - (sizeAll / 2);
+        float centerY = getHeight() / 2;
+
+        for (int i = 0; i < count; i++) {
+            if (i == positionIcon) {
+                indicatorList.add(new Icon(positionIcon == currentPosition ? selectedRes : unselectedRes
+                        , firstX + (i + 0.5f) * radius + i * DISTANCE, centerY - (radius / 2)
+                        , radius, currentPosition == 0 ? ALPHA_SELECTED : ALPHA_UNSELECTED));
+            } else {
+                indicatorList.add(new Circle(firstX + (i + 0.5f) * radius + i * DISTANCE,
+                        centerY, radius, i == currentPosition ? selectedColor : unselectedColor,
+                        i == currentPosition ? ALPHA_SELECTED : ALPHA_UNSELECTED));
+            }
+        }
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        if (indicatorList.size() == 0) {
+            return;
+        }
+
+        currentPosition = position;
+
+        postInvalidate();
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getSelectedColor() {
+        return selectedColor;
+    }
+
+    public void setSelectedColor(int selectedColor) {
+        this.selectedColor = selectedColor;
+    }
+
+    public int getUnselectedColor() {
+        return unselectedColor;
+    }
+
+    public void setUnselectedColor(int unselectedColor) {
+        this.unselectedColor = unselectedColor;
+    }
 }
